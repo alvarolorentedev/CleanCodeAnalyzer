dcbdd0f5d1511bf502e8c943a829733c255bc130
Kanekitoc <alvarojosepl@gmail.com>
2015-11-03 07:18:08 +0000

    tests outdated, need updatem and naming analizer implementation started

--- CleanCode/CleanCode/CleanCode.Test/UnitTests.cs
+++ CleanCode/CleanCode/CleanCode.Test/UnitTests.cs
@@ -9,76 +9,76 @@ using CleanCode.DiagnosticAnalizers;
 
 namespace CleanCode.Test
 {
-    [TestClass]
-    public class UnitTest : CodeFixVerifier
-    {
+    //[TestClass]
+    //public class UnitTest : CodeFixVerifier
+    //{
 
-        //No diagnostics expected to show up
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var test = @"";
+    //    //No diagnostics expected to show up
+    //    [TestMethod]
+    //    public void TestMethod1()
+    //    {
+    //        var test = @"";
 
-            VerifyCSharpDiagnostic(test);
-        }
+    //        VerifyCSharpDiagnostic(test);
+    //    }
 
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public void TestMethod2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
+    //    //Diagnostic and CodeFix both triggered and checked for
+    //    [TestMethod]
+    //    public void TestMethod2()
+    //    {
+    //        var test = @"
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Text;
+    //using System.Threading.Tasks;
+    //using System.Diagnostics;
 
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
-        }
-    }";
-            var expected = new DiagnosticResult
-            {
-                Id = "CleanCode",
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
-                Locations =
-                    new[] {
-                            new DiagnosticResultLocation("Test0.cs", 11, 15)
-                        }
-            };
+    //namespace ConsoleApplication1
+    //{
+    //    class TypeName
+    //    {   
+    //    }
+    //}";
+    //        var expected = new DiagnosticResult
+    //        {
+    //            Id = "CleanCode",
+    //            Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
+    //            Severity = DiagnosticSeverity.Warning,
+    //            Locations =
+    //                new[] {
+    //                        new DiagnosticResultLocation("Test0.cs", 11, 15)
+    //                    }
+    //        };
 
-            VerifyCSharpDiagnostic(test, expected);
+    //        VerifyCSharpDiagnostic(test, expected);
 
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
+    //        var fixtest = @"
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Text;
+    //using System.Threading.Tasks;
+    //using System.Diagnostics;
 
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-            VerifyCSharpFix(test, fixtest);
-        }
+    //namespace ConsoleApplication1
+    //{
+    //    class TYPENAME
+    //    {   
+    //    }
+    //}";
+    //        VerifyCSharpFix(test, fixtest);
+    //    }
 
-        protected override CodeFixProvider GetCSharpCodeFixProvider()
-        {
-            return null;
-            //return new CleanCodeCodeFixProvider();
-        }
+    //    protected override CodeFixProvider GetCSharpCodeFixProvider()
+    //    {
+    //        return null;
+    //        //return new CleanCodeCodeFixProvider();
+    //    }
 
-        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-        {
-            return new MethodMetricsAnalizer();
-        }
-    }
+    //    protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+    //    {
+    //        return new MethodMetricsAnalizer();
+    //    }
+    //}
 }
---
+++ CleanCode/CleanCode/CleanCode/DiagnosticAnalizers/NamingAnalizer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.DiagnosticAnalizers
+{
+    //[DiagnosticAnalyzer(LanguageNames.CSharp)]
+    //public class NamingAnalizer : DiagnosticAnalyzer
+    //{
+
+        //public const string DiagnosticId = "CleanCode.Naming.";
+
+        //const string Category = "Naming";
+
+        //public const string ComplexityId = "Complexity";
+        //static readonly LocalizableString ComplexityTitle = new LocalizableResourceString(nameof(Resources.ComplexityAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        //static readonly LocalizableString ComplexityMessageFormat = new LocalizableResourceString(nameof(Resources.ComplexityAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        //static readonly LocalizableString ComplexityDescription = new LocalizableResourceString(nameof(Resources.ComplexityAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+
+        //static DiagnosticDescriptor ComplexityRule = new DiagnosticDescriptor(string.Format("{0}{1}", DiagnosticId, ComplexityId), ComplexityTitle, ComplexityMessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: ComplexityDescription);
+        //public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(ComplexityRule); } }
+
+        //public override void Initialize(AnalysisContext context)
+        //{
+        //    context.RegisterCodeBlockAction(Analyzeblock);
+        //}
+
+        //async void Analyzeblock(CodeBlockAnalysisContext obj)
+        //{
+        //    //var settings = SettingsHelper.GetSettings(obj.Options);
+        //    var settings = new Settings();
+        //    CheckComplexity(obj, functionMetric.CyclomaticComplexity, settings.MethodSettings);
+        //}
+
+
+        //protected void CheckComplexity(CodeBlockAnalysisContext obj, int cyclomaticComplexity, IMethodSettings methodSettings)
+        //{
+        //    if (cyclomaticComplexity < 10)
+        //        return;
+
+        //    var diagnostic = Diagnostic.Create(ComplexityRule, obj.CodeBlock.GetLocation(), new object[] { "Cyclomatic Complexity", 10 });
+        //    obj.ReportDiagnostic(diagnostic);
+
+        //}
+
+    //}
+}
